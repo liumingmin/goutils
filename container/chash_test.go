c6a1450fc522cc74bbca50c4b1d86765d0709410
liumingmin <liumingmin@kingsoft.com>
2019-03-05 20:08:15 +0800

    go test

--- container/chash_test.go
+++ container/chash_test.go
@@ -1,6 +1,12 @@
 package container
 
-import "testing"
+import (
+	"testing"
+
+	"time"
+
+	"github.com/liumingmin/goutils/safego"
+)
 
 type Node string
 
@@ -42,4 +48,71 @@ func TestNewChash(t *testing.T) {
 
 	c.AddNode(Node("ccccc"))
 	t.Log(c.GetNode("fdsafdwfe"))
+}
+
+func TestNewChash2(t *testing.T) {
+	strs := []NodeHealth{Node("111"), Node("222"), Node("3333"), Node("4444"), Node("5555")}
+	var configs []interface{}
+	for _, str := range strs {
+		configs = append(configs, str)
+	}
+	c := NewChash(strs)
+	//t.Log(c.ring.Value)
+
+	//safego.Go(func() {
+	t.Log(c.GetNode("fdsafdwfe"))
+	//})
+
+	//safego.Go(func() {
+	c.AddNode(Node("66666"))
+	//})
+	//safego.Go(func() {
+	c.AddNode(Node("777777"))
+	//})
+	//safego.Go(func() {
+	//c.AddNode(Node("8888"))
+	//})
+	//safego.Go(func() {
+	//c.AddNode(Node("99999"))
+	//})
+
+	safego.Go(func() {
+		t.Log(c.GetNode("fdsafdwfe"))
+	})
+	//
+
+	safego.Go(func() {
+		t.Log(c.GetNode("fdsafdwfe"))
+	})
+
+	safego.Go(func() {
+		t.Log(c.GetNode("fdsafdwfe"))
+	})
+
+	safego.Go(func() {
+		t.Log(c.GetNode("fdsafdwfe"))
+	})
+
+	safego.Go(func() {
+		c.AddNode(Node("aaaaa"))
+	})
+
+	safego.Go(func() {
+		t.Log(c.GetNode("fdsafdwfe"))
+	})
+	safego.Go(func() {
+		c.AddNode(Node("bbbbb"))
+	})
+
+	safego.Go(func() {
+		t.Log(c.GetNode("fdsafdwfe"))
+	})
+	safego.Go(func() {
+		c.AddNode(Node("ccccc"))
+	})
+	safego.Go(func() {
+		t.Log(c.GetNode("fdsafdwfe"))
+	})
+
+	time.Sleep(time.Second)
 }
