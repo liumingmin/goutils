911c528f147315e797ea2978f9f65b0810d1fd63
liumingmin <443925902@qq.com>
2022-03-04 14:09:19 +0800

    feat: 补充mongo test

--- db/mongo/collection_test.go
+++ db/mongo/collection_test.go
@@ -8,18 +8,67 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func TestCompCollection(t *testing.T) {
+const dbKey = "testDbKey"
+const dbName = "testDb"
+const collectionName = "testUser"
+
+type testUser struct {
+	UserId   string `bson:"user_id"`
+	Nickname string `bson:"nick_name"`
+	Status   string `bson:"status"`
+	Type     string `bson:"p_type"`
+}
+
+func TestInsert(t *testing.T) {
 	ctx := context.Background()
 	InitClients()
-	c, err := MgoClient("goutils")
-	log.Error(ctx, "err :%v", err)
+	c, _ := MgoClient(dbKey)
 
-	op := NewCompCollectionOp(c, "confmgr", "test")
-	op.Insert(ctx, bson.M{"name": "test"})
+	op := NewCompCollectionOp(c, dbName, collectionName)
+	op.Insert(ctx, testUser{
+		UserId:   "1",
+		Nickname: "超级棒",
+		Status:   "valid",
+		Type:     "normal",
+	})
 
 	var result interface{}
 	op.FindOne(ctx, FindModel{
-		Query:   bson.M{},
+		Query:   bson.M{"user_id": "1"},
+		Results: &result,
+	})
+
+	log.Info(ctx, "result: %v", result)
+}
+
+func TestUpdate(t *testing.T) {
+	ctx := context.Background()
+	InitClients()
+	c, _ := MgoClient(dbKey)
+
+	op := NewCompCollectionOp(c, dbName, collectionName)
+	op.Update(ctx, bson.M{"user_id": "1"}, bson.M{"$set": bson.M{"nick_name": "超级棒++"}})
+
+	var result interface{}
+	op.FindOne(ctx, FindModel{
+		Query:   bson.M{"user_id": "1"},
+		Results: &result,
+	})
+
+	log.Info(ctx, "result: %v", result)
+}
+
+func TestDelete(t *testing.T) {
+	ctx := context.Background()
+	InitClients()
+	c, _ := MgoClient(dbKey)
+
+	op := NewCompCollectionOp(c, dbName, collectionName)
+	op.Delete(ctx, bson.M{"user_id": "1"})
+
+	var result interface{}
+	op.FindOne(ctx, FindModel{
+		Query:   bson.M{"user_id": "1"},
 		Results: &result,
 	})
 
