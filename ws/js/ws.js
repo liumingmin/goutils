2a12ceed087f103131544702fedce77e97a1ff25
liumingmin <443925902@qq.com>
2022-09-29 10:50:28 +0800

    feat: opt js client

--- ws/js/ws.js
+++ ws/js/ws.js
@@ -61,33 +61,4 @@ class WsConnection {
         wsMessage.setData(data);
         this.ws.send(wsMessage.serializeBinary());
     }
-
-    stringToArrayBuffer(s) {
-        var i = s.length;
-        var n = 0;
-        var ba = new Array()
-        for (var j = 0; j < i;) {
-            var c = s.codePointAt(j);
-            if (c < 128) {
-                ba[n++] = c;
-                j++;
-            } else if ((c > 127) && (c < 2048)) {
-                ba[n++] = (c >> 6) | 192;
-                ba[n++] = (c & 63) | 128;
-                j++;
-            } else if ((c > 2047) && (c < 65536)) {
-                ba[n++] = (c >> 12) | 224;
-                ba[n++] = ((c >> 6) & 63) | 128;
-                ba[n++] = (c & 63) | 128;
-                j++;
-            } else {
-                ba[n++] = (c >> 18) | 240;
-                ba[n++] = ((c >> 12) & 63) | 128;
-                ba[n++] = ((c >> 6) & 63) | 128;
-                ba[n++] = (c & 63) | 128;
-                j += 2;
-            }
-        }
-        return new Uint8Array(ba);
-    }
 }
