b5cb4bad3721097d59ad6cc93042ec30810ff556
liumingmin <443925902@qq.com>
2023-04-29 20:09:04 +0800

    feat: new bandwidth limit

---
+++ net/bwlimit/dialer.go
@@ -0,0 +1,86 @@
+package bwlimit
+
+import (
+	"context"
+	"net"
+	"time"
+)
+
+// Dialer structure contains default dialer and timeout
+type Dialer struct {
+	net.Dialer
+	rxBwLimit *BwLimit
+	txBwLimit *BwLimit
+
+	Timeout time.Duration
+}
+
+// NewDialer creates a Dialer structure with Timeout, Keepalive
+// bandwidth limit rx: read, tx write
+func NewDialer(ctx context.Context, rx, tx *BwLimit, timeout time.Duration) *Dialer {
+	dialer := &Dialer{
+		Dialer:    net.Dialer{},
+		rxBwLimit: rx,
+		txBwLimit: tx,
+
+		Timeout: timeout,
+	}
+	return dialer
+}
+
+// Dial connects to the network address.
+func (d *Dialer) Dial(network, address string) (net.Conn, error) {
+	return d.DialContext(context.Background(), network, address)
+}
+
+// DialContext connects to the network address using the provided context.
+func (d *Dialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	c, err := d.Dialer.DialContext(ctx, network, address)
+	if err != nil {
+		return c, err
+	}
+
+	con := &timeoutConn{
+		Conn:      c,
+		timeout:   d.Timeout,
+		rxBwLimit: d.rxBwLimit,
+		txBwLimit: d.txBwLimit,
+	}
+	return con, con.nudgeDeadline()
+}
+
+// A net.Conn that sets deadline for every Read/Write operation
+type timeoutConn struct {
+	net.Conn
+	timeout   time.Duration
+	rxBwLimit *BwLimit
+	txBwLimit *BwLimit
+}
+
+// Nudge the deadline for an idle timeout on by c.timeout if non-zero
+func (c *timeoutConn) nudgeDeadline() error {
+	if c.timeout > 0 {
+		return c.SetDeadline(time.Now().Add(c.timeout))
+	}
+	return nil
+}
+
+// Read bytes with rate limiting and idle timeouts
+func (c *timeoutConn) Read(b []byte) (n int, err error) {
+	n, err = c.Conn.Read(b)
+	c.rxBwLimit.LimitBandwidth(n)
+	if err == nil && n > 0 && c.timeout > 0 {
+		err = c.nudgeDeadline()
+	}
+	return n, err
+}
+
+// Write bytes with rate limiting and idle timeouts
+func (c *timeoutConn) Write(b []byte) (n int, err error) {
+	c.txBwLimit.LimitBandwidth(len(b))
+	n, err = c.Conn.Write(b)
+	if err == nil && n > 0 && c.timeout > 0 {
+		err = c.nudgeDeadline()
+	}
+	return n, err
+}
