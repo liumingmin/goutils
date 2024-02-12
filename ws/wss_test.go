f41655ce7e861ec2d023dcfeebece92079a96225
liumingmin <443925902@qq.com>
2024-02-12 17:43:40 +0800

    refactor: websocket test case

--- ws/wss_test.go
+++ ws/wss_test.go
@@ -35,19 +35,13 @@ func TestMessage(t *testing.T) {
 }
 
 func TestWssRequestResponse(t *testing.T) {
-	InitServerWithOpt(ServerOption{[]HubOption{HubShardOption(4)}}) //server invoke 服务端调用
-	InitClient()                                                    //client invoke 客户端调用
+	InitServerWithOpt(ServerOption{[]HubOption{HubShardOption(4)}}) //server invoke
+	InitClient()                                                    //client invoke
 	ctx := context.Background()
 
 	const (
 		C2S_REQ  = 2
 		S2C_RESP = 3
-
-		C2S_REQ_TIMEOUT  = 4
-		S2C_RESP_TIMEOUT = 5
-
-		S2C_REQ  = 6
-		C2S_RESP = 7
 	)
 
 	//server reg handler
@@ -55,15 +49,6 @@ func TestWssRequestResponse(t *testing.T) {
 		log.Info(ctx, "server recv: %v, %v", message.GetSn(), string(message.GetData()))
 		packet := GetPoolMessage(S2C_RESP)
 		packet.SetData([]byte("server rpc resp info"))
-		return connection.SendResponseMsg(ctx, packet, message.GetSn(), nil)
-	})
-
-	RegisterHandler(C2S_REQ_TIMEOUT, func(ctx context.Context, connection IConnection, message IMessage) error {
-		log.Info(ctx, "server recv: %v, %v", message.GetSn(), string(message.GetData()))
-
-		time.Sleep(time.Second * 4)
-		packet := GetPoolMessage(S2C_RESP_TIMEOUT)
-		packet.SetData([]byte("server rpc resp info timeout"))
 		return connection.SendResponseMsg(ctx, packet, message.GetSn(), nil)
 	})
 
@@ -84,12 +69,6 @@ func TestWssRequestResponse(t *testing.T) {
 			CompressionLevelOption(2),
 			ConnEstablishHandlerOption(func(ctx context.Context, conn IConnection) {
 				log.Info(ctx, "server conn establish: %v, %p", conn.Id(), conn)
-				//在集群环境下，需要检查connId是否已经连接集群，如有需踢掉在集群其他节点建立的连接，可通过redis pub sub，其他节点收到通知调用KickClient
-				//lastConnNodeId, lastConnMTs := GetClientTs(ctx, conn.Id())
-				//if lastConnNodeId != "" && lastConnNodeId != config.NodeId && lastConnMTs < util.UtcMTs() {
-				//	MqPublish(ctx, conn.Id(), conn.ClientIp())   //other node: ClientConnHub.Find(connId).DisplaceClientByIp(ctx, newIp)
-				//}
-				//RegisterConn() // save to redis
 			}),
 			ConnClosingHandlerOption(func(ctx context.Context, conn IConnection) {
 				log.Info(ctx, "server conn closing: %v, %p", conn.Id(), conn)
@@ -109,19 +88,6 @@ func TestWssRequestResponse(t *testing.T) {
 	RegisterHandler(S2C_RESP, func(ctx context.Context, connection IConnection, message IMessage) error {
 		log.Info(ctx, "client handler recv sn: %v, %v", message.GetSn(), string(message.GetData()))
 		return nil
-	})
-
-	RegisterHandler(S2C_RESP_TIMEOUT, func(ctx context.Context, connection IConnection, message IMessage) error {
-		log.Info(ctx, "client handler recv sn: %v, %v", message.GetSn(), string(message.GetData()))
-		return nil
-	})
-
-	RegisterHandler(S2C_REQ, func(ctx context.Context, connection IConnection, message IMessage) error {
-		log.Info(ctx, "client recv server request sn: %v, %v", message.GetSn(), string(message.GetData()))
-
-		packet := GetPoolMessage(C2S_RESP)
-		packet.SetData([]byte("client rpc response info"))
-		return connection.SendResponseMsg(ctx, packet, message.GetSn(), nil)
 	})
 
 	//client connect
@@ -144,7 +110,7 @@ func TestWssRequestResponse(t *testing.T) {
 		}),
 	)
 	log.Info(ctx, "%v", conn)
-	time.Sleep(time.Second * 5)
+	time.Sleep(time.Second * 1)
 
 	for i := 0; i < 10; i++ {
 		packet := GetPoolMessage(C2S_REQ)
@@ -154,6 +120,88 @@ func TestWssRequestResponse(t *testing.T) {
 			log.Info(ctx, "client recv: sn: %v, data: %v", resp.GetSn(), string(resp.GetData()))
 		}
 	}
+
+	time.Sleep(time.Second * 5)
+}
+
+func TestWssRequestResponseWithTimeout(t *testing.T) {
+	InitServerWithOpt(ServerOption{[]HubOption{HubShardOption(4)}}) //server invoke
+	InitClient()                                                    //client invoke
+	ctx := context.Background()
+
+	const (
+		C2S_REQ_TIMEOUT  = 4
+		S2C_RESP_TIMEOUT = 5
+	)
+
+	//server reg handler
+	RegisterHandler(C2S_REQ_TIMEOUT, func(ctx context.Context, connection IConnection, message IMessage) error {
+		log.Info(ctx, "server recv: %v, %v", message.GetSn(), string(message.GetData()))
+
+		time.Sleep(time.Second * 4)
+		packet := GetPoolMessage(S2C_RESP_TIMEOUT)
+		packet.SetData([]byte("server rpc resp info timeout"))
+		return connection.SendResponseMsg(ctx, packet, message.GetSn(), nil)
+	})
+
+	//server start
+	e := gin.New()
+	e.GET("/join", func(ctx *gin.Context) {
+		connMeta := ConnectionMeta{
+			UserId:   ctx.Query("uid"),
+			Typed:    0,
+			DeviceId: "",
+			Version:  0,
+			Charset:  0,
+		}
+		_, err := AcceptGin(ctx, connMeta, DebugOption(true),
+			SrvUpgraderCompressOption(true),
+			CompressionLevelOption(2),
+			ConnEstablishHandlerOption(func(ctx context.Context, conn IConnection) {
+				log.Info(ctx, "server conn establish: %v, %p", conn.Id(), conn)
+			}),
+			ConnClosingHandlerOption(func(ctx context.Context, conn IConnection) {
+				log.Info(ctx, "server conn closing: %v, %p", conn.Id(), conn)
+			}),
+			ConnClosedHandlerOption(func(ctx context.Context, conn IConnection) {
+				log.Info(ctx, "server conn closed: %v, %p", conn.Id(), conn)
+			}),
+		)
+		if err != nil {
+			log.Error(ctx, "Accept client connection failed. error: %v", err)
+			return
+		}
+	})
+	go e.Run(":8003")
+
+	//client reg handler
+	RegisterHandler(S2C_RESP_TIMEOUT, func(ctx context.Context, connection IConnection, message IMessage) error {
+		log.Info(ctx, "client handler recv sn: %v, %v", message.GetSn(), string(message.GetData()))
+		return nil
+	})
+
+	//client connect
+	uid := "100"
+	url := "ws://127.0.0.1:8003/join?uid=" + uid
+	conn, _ := DialConnect(context.Background(), url, http.Header{},
+		DebugOption(true),
+		ClientIdOption("server1"),
+		ClientDialWssOption(url, false),
+		ClientDialCompressOption(true),
+		CompressionLevelOption(2),
+		ConnEstablishHandlerOption(func(ctx context.Context, conn IConnection) {
+			log.Info(ctx, "client conn establish: %v, %p", conn.Id(), conn)
+		}),
+		ConnClosingHandlerOption(func(ctx context.Context, conn IConnection) {
+			log.Info(ctx, "client conn closing: %v, %p", conn.Id(), conn)
+		}),
+		ConnClosedHandlerOption(func(ctx context.Context, conn IConnection) {
+			log.Info(ctx, "client conn closed: %v, %p", conn.Id(), conn)
+		}),
+	)
+
+	log.Info(ctx, "%v", conn)
+	time.Sleep(time.Second * 1)
 
 	toCtx, _ := context.WithTimeout(ctx, time.Second*2)
 	packet := GetPoolMessage(C2S_REQ_TIMEOUT)
@@ -166,25 +214,12 @@ func TestWssRequestResponse(t *testing.T) {
 	}
 
 	time.Sleep(time.Second * 5)
-
-	connFromClient, _ := ClientConnHub.Find("100-0-")
-	for i := 0; i < 10; i++ {
-		packet := GetPoolMessage(S2C_REQ)
-		packet.SetData([]byte("server rpc req info"))
-		resp, err := connFromClient.SendRequestMsg(context.Background(), packet, nil)
-		if err == nil {
-			log.Debug(ctx, "server recv: sn: %v, data: %v", resp.GetSn(), string(resp.GetData()))
-		}
-	}
-
-	time.Sleep(time.Minute * 10)
-
 }
 
-func TestWssRun(t *testing.T) {
+func TestWssSendMessage(t *testing.T) {
 	//InitServer()
-	InitServerWithOpt(ServerOption{[]HubOption{HubShardOption(4)}}) //server invoke 服务端调用
-	InitClient()                                                    //client invoke 客户端调用
+	InitServerWithOpt(ServerOption{[]HubOption{HubShardOption(4)}}) //server invoke
+	InitClient()                                                    //client invoke
 	ctx := context.Background()
 
 	const (
@@ -239,7 +274,10 @@ func TestWssRun(t *testing.T) {
 			CompressionLevelOption(2),
 			ConnEstablishHandlerOption(func(ctx context.Context, conn IConnection) {
 				log.Info(ctx, "server conn establish: %v, %p", conn.Id(), conn)
-				//在集群环境下，需要检查connId是否已经连接集群，如有需踢掉在集群其他节点建立的连接，可通过redis pub sub，其他节点收到通知调用KickClient
+				// In a cluster environment, it is necessary to check whether connId has already connected to the cluster.
+				// If it is necessary to kick off connections established on other nodes in the cluster,
+				// it can be done through Redis pub sub, and other nodes will receive a notification to call KickClient
+
 				//lastConnNodeId, lastConnMTs := GetClientTs(ctx, conn.Id())
 				//if lastConnNodeId != "" && lastConnNodeId != config.NodeId && lastConnMTs < util.UtcMTs() {
 				//	MqPublish(ctx, conn.Id(), conn.ClientIp())   //other node: ClientConnHub.Find(connId).DisplaceClientByIp(ctx, newIp)
@@ -289,13 +327,14 @@ func TestWssRun(t *testing.T) {
 		}),
 	)
 	log.Info(ctx, "%v", conn)
-	time.Sleep(time.Second * 5)
+	time.Sleep(time.Second * 1)
 
+	//send msg
 	packet := GetPoolMessage(C2S_REQ)
 	packet.SetData([]byte("client request"))
 	conn.SendMsg(context.Background(), packet, nil)
 
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 1)
 
 	//client connect displace
 	conn2, _ := DialConnect(context.Background(), url, http.Header{},
@@ -308,13 +347,61 @@ func TestWssRun(t *testing.T) {
 			log.Info(ctx, "client conn establish: %v, %p", conn.Id(), conn)
 		}),
 	)
-	time.Sleep(time.Second)
+
+	time.Sleep(time.Second * 1)
+
+	//send msg by conn2
 	packet = GetPoolMessage(C2S_REQ)
 	packet.SetData([]byte("client request2"))
 	conn2.SendMsg(context.Background(), packet, nil)
 
-	clientConn, _ := ClientConnHub.Find("100-0-")
-	clientConn.DisplaceClientByIp(ctx, "newip")
+	time.Sleep(time.Second * 2)
+}
+
+func TestWssDialConnect(t *testing.T) {
+	InitServerWithOpt(ServerOption{[]HubOption{HubShardOption(4)}}) //server invoke
+	InitClient()                                                    //client invoke
+	//ctx := context.Background()
+
+	//server start
+	e := gin.New()
+	e.GET("/join", func(ctx *gin.Context) {
+		connMeta := ConnectionMeta{
+			UserId:   ctx.DefaultQuery("uid", ""),
+			Typed:    0,
+			DeviceId: "",
+			Version:  0,
+			Charset:  0,
+		}
+		_, err := AcceptGin(ctx, connMeta, DebugOption(true),
+			SrvUpgraderCompressOption(true),
+			CompressionLevelOption(2),
+			ConnEstablishHandlerOption(func(ctx context.Context, conn IConnection) {
+				log.Info(ctx, "server conn establish: %v, %p", conn.Id(), conn)
+				// In a cluster environment, it is necessary to check whether connId has already connected to the cluster.
+				// If it is necessary to kick off connections established on other nodes in the cluster,
+				// it can be done through Redis pub sub, and other nodes will receive a notification to call KickClient
+
+				//lastConnNodeId, lastConnMTs := GetClientTs(ctx, conn.Id())
+				//if lastConnNodeId != "" && lastConnNodeId != config.NodeId && lastConnMTs < util.UtcMTs() {
+				//	MqPublish(ctx, conn.Id(), conn.ClientIp())   //other node: ClientConnHub.Find(connId).DisplaceClientByIp(ctx, newIp)
+				//}
+				//RegisterConn() // save to redis
+			}),
+			ConnClosingHandlerOption(func(ctx context.Context, conn IConnection) {
+				log.Info(ctx, "server conn closing: %v, %p", conn.Id(), conn)
+			}),
+			ConnClosedHandlerOption(func(ctx context.Context, conn IConnection) {
+				log.Info(ctx, "server conn closed: %v, %p", conn.Id(), conn)
+			}))
+		if err != nil {
+			log.Error(ctx, "Accept client connection failed. error: %v", err)
+			return
+		}
+	})
+	go e.Run(":8003")
+
+	//client connect
 	for i := 0; i < 100; i++ {
 		url := "ws://127.0.0.1:8003/join?uid=a" + strconv.Itoa(i)
 		DialConnect(context.Background(), url, http.Header{},
@@ -326,27 +413,28 @@ func TestWssRun(t *testing.T) {
 					time.Sleep(time.Second * 3)
 					conn.KickServer()
 				})
-				log.Info(ctx, "client conn establish: %v", conn.Id())
+				//log.Info(ctx, "client conn establish: %v", conn.Id())
 			}),
 		)
 	}
+
 	fmt.Println(len(ClientConnHub.ConnectionIds()))
-	time.Sleep(time.Second * 5)
+	time.Sleep(time.Second * 3)
+
+	//client connect again
 	for i := 0; i < 100; i++ {
-		url := "ws://127.0.0.1:8003/join?uid=b" + strconv.Itoa(i)
+		url := "ws://127.0.0.1:8003/join?uid=a" + strconv.Itoa(i)
 		DialConnect(context.Background(), url, http.Header{},
 			DebugOption(true),
 			ClientIdOption("b"+strconv.Itoa(i)),
 			ClientDialWssOption(url, false),
 			ConnEstablishHandlerOption(func(ctx context.Context, conn IConnection) {
-				log.Info(ctx, "client conn establish: %v", conn.Id())
+				//log.Info(ctx, "client conn establish: %v", conn.Id())
 			}),
 		)
 	}
 
-	time.Sleep(time.Second * 5)
+	time.Sleep(time.Second * 3)
 
 	fmt.Println(len(ClientConnHub.ConnectionIds()))
-
-	time.Sleep(time.Minute * 1)
 }
